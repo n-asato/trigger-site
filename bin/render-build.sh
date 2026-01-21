d0c9033b8dff0315bdd262ae7c2a2a710b387d94
n-asato <na.124abema@gmail.com>
2026-01-22 00:51:29 +0900

    Add logging to build script for debugging

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -2,7 +2,12 @@
 # exit on error
 set -o errexit
 
+echo "--- Building assets ---"
 bundle install
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+
+echo "--- Running migrations ---"
 bundle exec rake db:migrate
+
+echo "--- Build complete ---"
