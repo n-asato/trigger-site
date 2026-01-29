e60d12da16d2cfd5e5127004b5df1d2a9e14d7b1
n-asato <na.124abema@gmail.com>
2026-01-29 14:16:59 +0900

    Update meta description to focus on care industry DX

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,7 +4,7 @@ module ApplicationHelper
       site: '愛知・名古屋の介護DX・介護IT 支援 TRiGGER｜ICTで介護現場の業務改善サポート',
       reverse: true,
       separator: '|',
-      description: '愛知県名古屋市名東区を拠点に、介護・福祉業界のデジタル化を推進するIT支援サービスTRiGGER。システムエンジニア経験を活かしたITコンサルタントとして、業務改善やPC環境整備、ホームページ制作、Web制作など、現場の"やりたい事"をITで実現します。',
+      description: '愛知県名古屋市名東区を拠点に、介護業界のDX・デジタル化を推進するIT支援サービスTRiGGER。システムエンジニア経験を活かしたITコンサルタントとして、業務改善やPC環境整備、ホームページ制作、Web制作など、現場の"やりたい事"をITで実現します。',
       keywords: '愛知県, 名古屋市, 名東区, 介護, 福祉, IT支援, ITコンサルタント, システムエンジニア, 業務改善, デジタル化, ホームページ制作, トリガー, TRiGGER',
       canonical: request.original_url,
       noindex: !Rails.env.production?,
@@ -15,7 +15,7 @@ module ApplicationHelper
       og: {
         site_name: '愛知・名古屋の介護DX・介護IT 支援 TRiGGER',
         title: '愛知・名古屋の介護DX・介護IT 支援 TRiGGER｜ICTで介護現場の業務改善サポート',
-        description: '愛知県名古屋市名東区を拠点に、介護・福祉業界のデジタル化を推進するIT支援サービスTRiGGER。システムエンジニア経験を活かしたITコンサルタントとして、ホームページ制作や業務改善をサポートします。',
+        description: '愛知県名古屋市名東区を拠点に、介護業界のDX・デジタル化を推進するIT支援サービスTRiGGER。システムエンジニア経験を活かしたITコンサルタントとして、ホームページ制作や業務改善をサポートします。',
         type: 'website',
         url: request.original_url,
         image: '/apple-touch-icon.png',
