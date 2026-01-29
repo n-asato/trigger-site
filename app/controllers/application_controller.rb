bb854ae5d69a33f611efe0b758337036b15d0112
n-asato <na.124abema@gmail.com>
2026-01-29 18:46:33 +0900

    Add 301 redirect from Render URL to canonical domain

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :redirect_to_canonical_domain
+
+  private
+
+  def redirect_to_canonical_domain
+    canonical_host = 't-rigger.net'
+    if Rails.env.production? && request.host != canonical_host
+      redirect_to "https://#{canonical_host}#{request.fullpath}", status: :moved_permanently, allow_other_host: true
+    end
+  end
 end
